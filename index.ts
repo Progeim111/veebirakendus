c14dcc441da36f807d9b704a43cc30e6a47b2ed3
raitsOOT <Rait.soot@voco.ee>
2024-07-10 20:46:47 +0300

    Parimad praktikad

--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import stringsController from "./controllers/strings";
+import productsController from "./controllers/products";
+import productListController from "./controllers/productlist";
 
 const app: Express = express();
 
-import stringsController from "./controllers/strings";
-import productsController from "./controllers/products";
-import productListsController from "./controllers/productlist";
-
-app.use('/', stringsController);
-app.use('/', productsController);
-app.use('/', productListsController);
-
-
 app.use(cors({
-    origin: ['http://localhost:3006']
-  }));
+  origin: ['http://localhost:3006']
+}));
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+app.use('/', stringsController);
+app.use('/', productsController);
+app.use('/', productListController);
 
 app.listen(3000,() => {
   console.log(`[server]: Server is running at http://localhost:3000`);
