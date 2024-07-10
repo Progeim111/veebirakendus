2fa2a117338dee08a1ea99549a1ad09794d8f2ac
raitsOOT <Rait.soot@voco.ee>
2024-07-10 21:05:23 +0300

    API päring teise rakendusse: Elering päringu modifitseerimine

--- controllers/nordpool.ts
+++ controllers/nordpool.ts
@@ -5,9 +5,24 @@ const router: Router = Router();
 
 router.get("/nord-pool-price", async (req: Request, res: Response) => {
   const response = await axios.get(
-    "https://dashboard.elering.ee/api/nps/price"
+    "https://dashboard.elering.ee/api/nps/price?start="+req.query.start+"&end="+req.query.end
   );
-  res.json(response.data);
+  switch(req.params.country) {
+    case "ee":
+      res.json(response.data.data.ee);
+      break;
+    case "lv":
+      res.json(response.data.data.lv);
+      break;
+    case "lt":
+      res.json(response.data.data.lt);
+      break;
+    case "fi":
+      res.json(response.data.data.fi);
+      break;
+    default:
+      res.json([]);
+  }
 });
 
 export default router;
